ee8f9fd888a88193f98777e4b65967cae04f1ef4
William Golden <wtgolden5@gmail.com>
2024-03-03 16:26:15 -0500

    require api key

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -14,13 +16,21 @@ func main() {
 		fmt.Println("Error loading .env file")
 	}
 	router := gin.Default()
+	apiKey := os.Getenv("API_KEY")
 
 	router.GET("/rebalance", func(ctx *gin.Context) {
+		requestKey := ctx.GetHeader("X-API-KEY")
+		if requestKey != apiKey {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid API key"})
+			return
+		}
+
 		activeStrategyId := ctx.Query("activeStrategyId")
 
 		go rebalance.RebalanceUserPortfolio(activeStrategyId)
+		fmt.Println("rebalance initiated")
 		ctx.JSON(200, gin.H{
-			"message": "hit",
+			"data": "rebalance initiated",
 		})
 	})
 
